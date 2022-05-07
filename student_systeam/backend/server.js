bc7cb3b7a5cdd5a76904964bde6704bd3ca93558
Yomal-Dulanjana <65000757+Yomal-Dulanjana@users.noreply.github.com>
2022-05-07 16:13:48 +0530

    quiz function done

--- student_systeam/backend/server.js
+++ student_systeam/backend/server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(cors());
-app.use('/quizes', quizRoute)
+app.use('/quizs', quizRoute)
 
 // PORT
 const port = process.env.PORT || 8070;
